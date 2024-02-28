744aaddcde6331807ca0489a8f6576cf3a433b0d
Richard Jean-Baptiste <Richinbk1@gmail.com>
2024-02-28 18:02:48 -0500

    moved request.url outside of try/catch block

--- app/api/get_image/route.ts
+++ app/api/get_image/route.ts
@@ -4,6 +4,10 @@ import { imgfile } from "../Schemas";
 
 
 export async function GET(request: NextRequest){
+    
+    const url = new URL(request.url);
+    const objectId = url.searchParams.get("id");
+    
     try {
         let mongo_uri:string | undefined = process.env.NEXT_PUBLIC_MONGO_URI;
 
@@ -11,14 +15,6 @@ export async function GET(request: NextRequest){
             await mongoose.connect(mongo_uri);
         }
         
-        const url = new URL(request.url);
-        const objectId = url.searchParams.get("id");
-
-
-        if (!objectId) {
-            return NextResponse.error();
-        }
-
 
         let query = await imgfile.findOne({_id: objectId});
     
